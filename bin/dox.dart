27d6d8020f0fc0f58a848eeeb53f03affcc1b0c5
AJ <aj.zinkyaw@gmail.com>
2023-05-06 13:15:10 +0700

    release 44

--- bin/dox.dart
+++ bin/dox.dart
@@ -18,7 +18,7 @@ void main(List<String> args) async {
   ];
 
   if (args.length == 1 && versionKeys.contains(args[0])) {
-    print('Dox version: 1.0.43');
+    print('Dox version: 1.0.44');
   }
 
   if (args.length == 2 && args[0] == 'create') {
